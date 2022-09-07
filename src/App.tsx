3fa9414c23bc5bb2a488188a2179ba3b3a40f653
leah <theloadingpage@gmail.com>
2022-09-07 18:04:42 +0100

    format and lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import babyData from "./babyNamesData.json"
+import babyData from "./babyNamesData.json";
 
 interface IBaby {
-    id: number,
-    name: string,
-    sex: string
-  }
-
+  id: number;
+  name: string;
+  sex: string;
+}
 
 function App(): JSX.Element {
+  const babyNamesList = babyData.map((baby: IBaby) => {
+    return (
+      <li className={baby.sex === "f" ? "female" : "male"} key={baby.id}>
+        {baby.name}
+      </li>
+    );
+  });
 
-  const babyNamesList = babyData.map((baby: IBaby) => { 
-  return <li className={baby.sex === "f" ? "female" : "male"} key={baby.id}>{baby.name}</li>
-  })
-  
-  return <ul>{babyNamesList}</ul>
+  return <ul>{babyNamesList}</ul>;
 }
 
 export default App;
