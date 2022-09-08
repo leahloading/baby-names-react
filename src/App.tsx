ca88c5410f3678a07870d63d094b5c5b15e7ebf7
leah <theloadingpage@gmail.com>
2022-09-09 00:14:47 +0100

    create search bar state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import "./app.css"
 import BabyNamesSection from "./components/BabyNamesSection";
+import SearchBar from "./components/SearchBar";
 
 
 function App(): JSX.Element {
 
-  return <><BabyNamesSection/>
-          </>
+  const [userInput, setUserInput] = useState("");
+
+  return <>
+  <h1>Baby Names</h1>
+  <SearchBar
+  userInput={userInput}
+  onType={setUserInput}/>
+
+  <BabyNamesSection/>
+  </>
 }
 
 export default App;
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,21 @@
+import React, { Dispatch, SetStateAction } from "react";
+
+interface SearchBarProps {
+    userInput: string,
+    onType: Dispatch<SetStateAction<string>>
+}
+
+function SearchBar({userInput, onType}:SearchBarProps):JSX.Element {
+
+    function handleUserInput(e:React.ChangeEvent<HTMLInputElement>){
+        onType(e.target.value)
+        console.log(userInput);
+    }
+    return <>
+    <input type="text" 
+    value={userInput}
+    onChange={handleUserInput}/> Search 
+    </>
+}
+
+export default SearchBar;
